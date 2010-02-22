6f15b534676f0a4a5f264286c0a0a46c4d053d0b
phil <phil@phil.(none)>
2010-02-22 14:43:54 +0000

    2nd attempt at a push

---
+++ src/test/scala/com/company/controller/MyControllerTest.scala
@@ -0,0 +1,27 @@
+package com.company.controller
+
+import org.junit.Assert._
+import org.junit.Test
+import com.company.service.MyService
+
+class MyControllerTest {
+
+  val controller = new MyController(new MyService)
+
+  @Test
+  def putAndGetAnIOU = {
+    controller.post("aaa","bbb",10d)
+    val json1 = controller.get("aaa")
+    assertEquals("""[{"amount":10,"ower":"aaa","owed":"bbb"}]""",json1)
+  }
+
+  @Test
+  def getOrderedIOUs = {
+    controller.post("aaa","bbb", 10d)
+    controller.post("aaa","ccc", 11d)
+    controller.post("aaa","ddd", 9d)
+    val json1 = controller.get("aaa")
+    assertEquals("""[{"amount":11,"ower":"aaa","owed":"ccc"},{"amount":10,"ower":"aaa","owed":"bbb"},{"amount":9,"ower":"aaa","owed":"ddd"}]""",json1)
+  }
+
+}
