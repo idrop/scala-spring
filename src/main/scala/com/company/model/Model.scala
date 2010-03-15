a9609c98c7fbdab84ed8950fb1fd4c20651cfe21
phil <phil@phil.(none)>
2010-03-15 15:57:54 +0000

    updated hashcode for an IOU

--- src/main/scala/com/company/model/Model.scala
+++ src/main/scala/com/company/model/Model.scala
@@ -8,12 +8,15 @@ case class IOU(ower: User, owed: User, amount: Double) {
 
   require(amount > 0)
 
+  /**
+   * One IOU equals another if ower and owed are the same
+   */
   override def equals(a: Any) = a match {
     case other: IOU => other.ower == ower && other.owed == owed
     case _ => false
   }
 
-  override def hashCode = ower.hashCode * owed.hashCode
+  override def hashCode = 41 * (41 + ower.hashCode) + owed.hashCode
 
   override def toString = "%s owes %s %s".format(ower, owed, amount)
 
